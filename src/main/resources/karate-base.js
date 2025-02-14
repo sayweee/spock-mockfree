67ef1eeab09375e1d2b5753c4a39f9798bb72480
Fei He <fei.he@sayweee.com>
2025-02-14 15:48:53 +0800

    deploy es query

--- src/main/resources/karate-base.js
+++ src/main/resources/karate-base.js
@@ -2,6 +2,7 @@ function fn() {
     karate.configure('connectTimeout', 10000);
     karate.configure('readTimeout', 10000);
     let DataBasePreparer = Java.type('com.sayweee.spock.mockfree.karate.DataBasePreparer');
+    let ElasticsearchPreparer = Java.type('com.sayweee.spock.mockfree.karate.ElasticsearchPreparer');
     let RedisPreparer = Java.type('com.sayweee.spock.mockfree.karate.RedisPreparer');
     let JwtPreparer = Java.type('com.sayweee.spock.mockfree.karate.JwtPreparer');
     let ServicePreparer = Java.type('com.sayweee.spock.mockfree.karate.ServicePreparer');
@@ -12,6 +13,7 @@ function fn() {
         baseUrl: 'http://localhost:' + local_server_port,
         apiBaseUrl: api_base_url,
         db: new DataBasePreparer(),
+        es: new ElasticsearchPreparer(),
         jwt: new JwtPreparer(),
         redis: new RedisPreparer(),
         service: new ServicePreparer(),
